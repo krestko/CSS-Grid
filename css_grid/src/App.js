0bf7f85cdca39f73c85ceb805759c7c9440ecafd
krestko <Katherine.V.Restko@gmail.com>
2018-12-25 01:45:56 -0600

    red grid gradient

---
+++ css_grid/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='Grid-container'>
+        <div className='Grid-item-1'>1</div>
+        <div className='Grid-item-2'>2</div>
+        <div className='Grid-item-3'>3</div>
+        <div className='Grid-item-4'>4</div>
+        <div className='Grid-item-5'>5</div>
+        <div className='Grid-item-6'>6</div>
+      </div>
+    );
+  }
+}
+
+export default App;

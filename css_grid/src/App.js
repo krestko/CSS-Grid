aa9b65468e396270d267bab0a876d3ec80181fc3
krestko <Katherine.V.Restko@gmail.com>
2018-12-25 03:08:57 -0600

    blue grid gradient with hover effect

--- css_grid/src/App.js
+++ css_grid/src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import FirstLine from './components/FirstLine/FirstLine';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className='Grid-container'>
-        <div className='Grid-item-1'>1</div>
-        <div className='Grid-item-2'>2</div>
-        <div className='Grid-item-3'>3</div>
-        <div className='Grid-item-4'>4</div>
-        <div className='Grid-item-5'>5</div>
-        <div className='Grid-item-6'>6</div>
+        <div className='Grid-item-1'><FirstLine /></div>
+        <div className='Grid-item-2'></div>
+        <div className='Grid-item-3'></div>
+        <div className='Grid-item-4'></div>
+        <div className='Grid-item-5'></div>
+        <div className='Grid-item-6'></div>
+        <div className='Grid-item-7'></div>
       </div>
     );
   }
---
+++ css_grid/src/components/FirstLine/FirstLine.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../../App.css';
+import './FirstLine.css';
+
+class FirstLine extends Component {
+  render() {
+    return (
+      <div className='Grid-item-1'>
+        <div className='First-Line-1'>Boop</div>
+        <div className='First-Line-2'>Boop</div>
+        <div className='First-Line-3'>Boop</div>
+      </div>
+    );
+  }
+}
+
+export default FirstLine;
